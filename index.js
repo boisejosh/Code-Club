7e50cfad268663215bc2822ed254c41ec570d549
josh <joshcrosby@yahoo.com>
2022-09-12 10:49:30 -0600

    10:49am

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-export default { fetch() {
-		return new Response('Hello JOSH!', {
-			headers: {
-				'content-type': 'text/plain',
-			},
-		});
-	},
-};
+export default {
+    fetch(request) {
+        if (request.method == "POST") {
+          return new Response("Hello POST, 9/12/2022!", {
+            headers: {
+              "content-type": "text/plain"
+            }
+          });
+        } else {
+          return new Response("This is a HTTP not a POST!", {
+            headers: {
+              "content-type": "text/plain"
+            }
+          });
+        }
+      }
+    };
+
